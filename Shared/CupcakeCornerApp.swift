9fb60c9c163d99b48b3c983353d9df024fb6f550
Erik Mikac <mikacew@gmail.com>
2021-04-11 09:07:35 -0400

    Initial Commit

---
+++ Shared/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  Shared
+//
+//  Created by Erik Mikac on 4/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
